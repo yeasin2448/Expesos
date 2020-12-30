391e182e7cc6e971355dfb29fbe309c501beb641
yeasin2448 <ayeasin108@gmail.com>
2020-12-30 22:58:37 +0600

    last card || && details

--- assets/js/app-ecommerce-details.js
+++ assets/js/app-ecommerce-details.js
@@ -31,22 +31,3 @@ $(document).ready(function () {
     $this.addClass('selected').siblings().removeClass('selected');
   })
 })
-
-// for tab content js start //
-function openCity(evt, cityName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-	tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-	tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(cityName).style.display = "block";
-	evt.currentTarget.className += " active";
-}
-	
-// Get the element with id="defaultOpen" and click on it
-	document.getElementById("defaultOpen").click();
-// for tabcontent js start //
